b96bf434a0860a7b6d9afe2a2ee098a5937dbc45
PabloHernandezUDC <125274113+PabloHernandezUDC@users.noreply.github.com>
2023-10-25 13:56:00 +0200

    y lo muestra

--- source.py
+++ source.py
@@ -7,9 +7,11 @@
 # statsmodels
 
 # cosas útiles:
-# https://realpython.com/linear-regression-in-python/
+# -> cómo hacer la refresión lineal y mostrarla:
+#   -> https://realpython.com/linear-regression-in-python/
+#   -> https://medium.com/analytics-vidhya/simple-linear-regression-with-example-using-numpy-e7b984f0d15e
+# -> documentación de plot(): https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html
 
-import sys
 import pandas as p
 import numpy as np
 import matplotlib.pyplot as plt
@@ -31,10 +33,14 @@ def ask(text, range):
 
     return result
 
-def r(e, n):
-    return round(e, n)
-
-print('hola gilberto')
+def abline(slope, intercept):
+    """
+    robado de: https://stackoverflow.com/questions/7941226/how-to-add-line-based-on-slope-and-intercept
+    """
+    axes = plt.gca()
+    x_vals = np.array(axes.get_xlim())
+    y_vals = intercept + slope * x_vals
+    plt.plot(x_vals, y_vals, '-r') # fmt = '[marker][line][color]'
 
 data = p.read_csv('modelos/housing.csv')
 #data = p.read_excel('modelos/housing.xlsx')
@@ -45,10 +51,10 @@ for c in data.columns:
     print(f'Columna número {i}: {c}')
     i += 1
 
-column1Index = ask('Selecciona la primera columna: ', 10)
-column2Index = ask('Selecciona la segunda columna: ', 10)
+column1Index = ask('Selecciona la primera columna: ', nOfColumns)
+column2Index = ask('Selecciona la segunda columna: ', nOfColumns)
 while column2Index == column1Index:
-    column2Index = ask('Selecciona la segunda columna: ', 10)
+    column2Index = ask('Selecciona la segunda columna: ', nOfColumns)
 
 selectedColumns = data.iloc[:, [column1Index, column2Index]]
 
@@ -59,8 +65,17 @@ model = LinearRegression().fit(x, y)
 
 intercept = model.intercept_ # término independiente
 slope = model.coef_[0]
+eq = f'{round(slope, 2)}x ' + ('+' if intercept > 0 else '-') + f' {round(abs(intercept), 2)}'
 
-print(f'la recta de regresión es {r(slope, 2)}x + {r(intercept, 2)}')
+print(f'la recta de regresión es', eq)
 
-r_sq = model.score(x, y)
-print(f"r cuadrado: {r(r_sq, 2)}")
+r_sq = round(model.score(x, y), 2)
+print(f"r cuadrado: {r_sq}")
+
+plt.plot(x, y, '.k')
+plt.ylabel(selectedColumns.columns[1])
+plt.xlabel(selectedColumns.columns[0])
+abline(slope, intercept)
+plt.title(eq + f',   r^2: {r_sq}')
+plt.grid()
+plt.show()
