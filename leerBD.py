1a010027fa9c2383a6fe43804997b7eed9368e28
Claudia <claudia.garcia.lopez@udc.es>
2023-11-09 21:28:34 +0100

    saca el nombre de la tabla con funcion

--- leerBD.py
+++ leerBD.py
@@ -6,12 +6,40 @@ def createDB(nombre_db):
     conexion.commit()
     conexion.close()
 
-def readRows(nombre_db, lista_columnas, nombre_tabla):
+def nombre_tabla(nombre_db):
+    conexion = sql.connect(nombre_db)
+    cursor = conexion.cursor()
+    
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tabla = cursor.fetchone()[0]
+
+    conexion.close()
+    
+    return tabla
+
+def nombre_columnas(nombre_db):
+    tabla = nombre_tabla(nombre_db)
+    
     conexion = sql.connect(nombre_db)
     cursor = conexion.cursor()
 
-    columnas = ', '.join(lista_columnas)
-    instruccion = "SELECT {} FROM {}".format(columnas, nombre_tabla)
+    cursor.execute(f"PRAGMA table_info({tabla});")
+
+    columnas = cursor.fetchall()
+
+    for columna in columnas:
+        print(columna[1])
+
+    conexion.close()
+
+
+def readRows(nombre_db):
+    conexion = sql.connect(nombre_db)
+    cursor = conexion.cursor()
+
+    tabla = nombre_tabla(nombre_db)
+
+    instruccion = "SELECT * FROM {}".format(tabla) 
     cursor.execute(instruccion)
     
     datos = cursor.fetchall() #crea una lista de tuplas con la info de la tabla
@@ -20,15 +48,16 @@ def readRows(nombre_db, lista_columnas, nombre_tabla):
     conexion.close()
     
     #for fila in datos:
-    #    print(fila)
+        #print(fila)
     return datos
 
-def readOrdered(nombre_db, lista_columnas, nombre_tabla,  field):
+def readOrdered(nombre_db, field):
     conexion = sql.connect(nombre_db)
     cursor = conexion.cursor()
 
-    columnas = ', '.join(lista_columnas)
-    instruccion = "SELECT {} FROM {} ORDER BY {}".format(columnas, nombre_tabla, field)
+    tabla = nombre_tabla(nombre_db)
+
+    instruccion = "SELECT * FROM {} ORDER BY {}".format(tabla, field)
     cursor.execute(instruccion)
     
     datos = cursor.fetchall() #crea una lista de tuplas con la info de la tabla
@@ -41,9 +70,8 @@ def readOrdered(nombre_db, lista_columnas, nombre_tabla,  field):
     return datos
 
 if __name__ == '__main__':
+    
     nombre = 'modelos/housing.db'
-    col = ['longitude', 'latitude', 'housing_median_age', 'total_rooms', 'total_bedrooms', 'population', 'households', 'median_income', 'median_house_value', 'ocean_proximity']
-    tabla = 'california_housing_dataset'
-   
-    readRows(nombre, col, tabla)
-    #readOrdered(nombre, col, tabla, 'longitude')
+    
+    readRows(nombre)
+    
